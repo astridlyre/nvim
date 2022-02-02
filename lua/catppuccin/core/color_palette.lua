cbe7fa5cc0689051dd057fbaa04990c78168446b
Erin Burton <eb@flat9.dev>
2022-02-02 00:16:36 -0800

    revert

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,26 +1,26 @@
 local color_palette = {
-	rosewater = "#e9e2e6", -- Rosewater
-	flamingo = "#e0d7dc", -- Flamingo
-	mauve = "#d7ccd2", -- Mauve
-	pink = "#cec0c8", -- Pink
+	rosewater = "#F5E0DC", -- Rosewater
+	flamingo = "#F2CDCD", -- Flamingo
+	mauve = "#DDB6F2", -- Mauve
+	pink = "#F5C2E7", -- Pink
 	red = "#F28FAD", -- Red
-	maroon = "#c5b5be", -- Maroon
-	peach = "#bca9b4", -- Peach
+	maroon = "#E8A2AF", -- Maroon
+	peach = "#F8BD96", -- Peach
 	yellow = "#FAE3B0", -- Yellow
-	green = "#f2eef0", -- Green
-	blue = "#b39eaa", -- Blue
+	green = "#ABE9B3", -- Green
+	blue = "#96CDFB", -- Blue
 	sky = "#89DCEB", -- Sky
 	teal = "#B5E8E0", -- Teal
-	lavender = "#fbf9fa", -- Lavender
-	white = "#e0d7dc", -- White
-	gray2 = "#6d5563", -- Gray2
-	gray1 = "#614c59", -- Gray1
-	gray0 = "#4a3a44", -- Gray0
-	black4 = "#33282f", -- Black4
-	black3 = "#281f25", -- Black3
-	black2 = "#171215", -- Black2
-	black1 = "#1d161a", -- Black1
-	black0 = "#110d10", -- Black0
+	lavender = "#C9CBFF", -- Lavender
+	white = "#D9E0EE", -- White
+	gray2 = "#C3BAC6", -- Gray2
+	gray1 = "#988BA2", -- Gray1
+	gray0 = "#6E6C7E", -- Gray0
+	black4 = "#575268", -- Black4
+	black3 = "#302D41", -- Black3
+	black2 = "#1E1E2E", -- Black2
+	black1 = "#1A1826", -- Black1
+	black0 = "#161320", -- Black0
 }
 
 return color_palette
